aeae4df976b9e9a69658f5ea75cc319b6b8afeb9
Jakub Pastuszka <jaku6p@gmail.com>
2025-08-14 15:16:59 +0200

    First assembler program

---
+++ Lab/lab_1/projekt_1/projekt_1/zadanie32.asm
@@ -0,0 +1,31 @@
+; Program przyk³adowy w trybie 32-bitowym
+
+.686
+.model flat
+
+extern	_ExitProcess@4	:PROC
+extern	__write			:PROC
+public  _main
+
+.data
+	tekst	db	10, 'Nazywam sie Jakub Pastuszka', 10						;29
+			db	'Moj pierwszy program assemblerowy dziala poprawnie!', 10	;52
+
+.code
+_main PROC
+
+	mov		ecx, 29+52				; dlugosc tekstu
+
+	; Wywwolanie funkcji __write
+	push	ecx						; dlugosc tekstu
+	push	dword PTR OFFSET tekst	; polozenie tekstu
+	push	dword PTR 1				; uchwyt urz¹zenia (stdout)
+	call	__write					; wywolanie funkcji __write
+	add		esp,12					; usuniecie argumentow ze stosu
+
+	push	dword PTR 0				; kod powrotu
+	call	_ExitProcess@4			; wywolanie funkcji ExitProcess
+
+_main ENDP
+END
+			
